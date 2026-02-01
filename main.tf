581f5f01045fb7b4316c38ad7743f30d1b911b5b
CyberCircuit <126124789+CyberCircuit@users.noreply.github.com>
2026-02-01 11:10:13 +1100

    load balancer

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ module "blog_alb" {
   vpc_id  = module.blog_vpc.vpc_id
   subnets = moduel.blog_vpc.public_subnets
 
-  secrity_groups = [module.blog_sg.security_group_id]
+  security_groups = [module.blog_sg.security_group_id]
 
   listeners = {
     blog-http = {
